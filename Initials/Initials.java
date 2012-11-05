083f2fde4fa38c8bbda02f97d795cf82e2c7951f
unknown <bmurph05@cspc052.dcs.bbk.ac.uk>
2012-11-05 14:24:15 +0000

    Trying to fix

---
+++ Initials/Initials.java
@@ -0,0 +1,16 @@
+public class Initials {
+
+	public String getInitials(String fullName) {
+		String result = "";
+		String[] words = fullName.split(" +");
+		for (int i = 0; i < words.length; i++) {
+		String nextInitial = "" + words[i].charAt(0);
+		result = result + nextInitial.toUpperCase();
+		}
+		return result;
+		}
+
+
+
+}
+
